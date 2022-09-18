825e06da148942523ab3ea7ca00596b75f33922a
yibaebi <elliot.yibaebi@yahoo.com>
2022-09-18 02:07:57 +0100

    add application assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+import boatIsland from './boat_island.png'
+import clientImage from './client_image.png'
+import contactUsIllustration from './contact_us__image.png'
+import expertServices from './expert_services.png'
+import servicesCover from './services_cover.png'
+import uburu from './uburu.png'
+import youFood from './you_food.png'
+
+export { expertServices, uburu, youFood, boatIsland, servicesCover, clientImage, contactUsIllustration }
---
+++ src/assets/svg/index.js
@@ -0,0 +1,39 @@
+import { ReactComponent as ActionIcon } from './action_icon.svg'
+import { ReactComponent as BrandFive } from './brand_five.svg'
+import { ReactComponent as BrandFour } from './brand_four.svg'
+import { ReactComponent as BrandOne } from './brand_one.svg'
+import { ReactComponent as BrandThree } from './brand_three.svg'
+import { ReactComponent as BrandTwo } from './brand_two.svg'
+import { ReactComponent as GlobeIcon } from './globe_icon.svg'
+import { ReactComponent as HamburgerMenu } from './hamburger_icon.svg'
+import { ReactComponent as LinkIcon } from './link_icon.svg'
+import { ReactComponent as LogoIpsumColored } from './logo_ipsum__colored.svg'
+import { ReactComponent as PrimaryLogo } from './primary_logo.svg'
+import { ReactComponent as LogoBlackAndWhite } from './primary_logo__black.svg'
+import { ReactComponent as ServiceAltPower } from './service_alt_power.svg'
+import { ReactComponent as ServiceBranding } from './service_branding.svg'
+import { ReactComponent as ServiceDesign } from './service_design.svg'
+import { ReactComponent as ServiceEngineering } from './service_engineering.svg'
+import { ReactComponent as ServiceNetworking } from './service_networking.svg'
+import { ReactComponent as ServiceSurveillance } from './service_surveillance.svg'
+
+export {
+  HamburgerMenu,
+  LogoBlackAndWhite,
+  ActionIcon,
+  LogoIpsumColored,
+  PrimaryLogo,
+  GlobeIcon,
+  BrandOne,
+  BrandTwo,
+  BrandThree,
+  BrandFour,
+  BrandFive,
+  ServiceAltPower,
+  ServiceBranding,
+  ServiceDesign,
+  ServiceEngineering,
+  ServiceNetworking,
+  ServiceSurveillance,
+  LinkIcon,
+}
